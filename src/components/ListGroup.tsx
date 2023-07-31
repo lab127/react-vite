443c41cc85e81b4e6313644a825990c3f0f1d26f
unknown <ronie2rush@gmail.com>
2023-07-31 17:46:34 +0700

    p1.3.8 passing data via props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  // hook (-1) initial value yang nanti berubah-ubah
-  // index numerik range 0-4
-  // jika gunakan nilai (1) maka "San Francisco" otomatis aktif saat pertama kali refresh
-  // maka gunakan nilai negatif (-1) agar tidak terpilih di awal
-  // value useSate selalu array ada 2 [initail value, value diubah]
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
