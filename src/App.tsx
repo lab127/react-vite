616e8f591e2fa0270e1d7972510268066f5adb54
unknown <ronie2rush@gmail.com>
2023-08-01 17:40:31 +0700

    p1.3.14 button toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // function saat list diklik
+  const handelSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  // status saat di klik
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const visibilityToggle = () => {
+    // current adalah arg fungsi setAlertVisibility
+    // jadi bisa apa aja
+    setAlertVisibility((current) => !current);
+  };
+
   return (
-    <div>
-      <Alert>
-        Hello <strong>World</strong>
-      </Alert>
-      <Button color="success" onClick={() => console.log("Klik")}>
+    <>
+      <div>
+        <ListGroup
+          items={items}
+          heading="Cities"
+          onSelectItem={handelSelectItem}
+        />
+      </div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="success" onClick={visibilityToggle}>
         Primary
       </Button>
-    </div>
+    </>
   );
 }
 
