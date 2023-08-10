4a0e7f021bacc6a53bc2e99a592e726785deb08b
unknown <ronie2rush@gmail.com>
2023-08-10 07:03:27 +0700

    p.6.4 accessing input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,24 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 const Form = () => {
+  // typescript compiler tidak tau yang diferensikan apa aja
+  // karena menggunaka useRef hook bisa mereferensikan apa aja di DOM, tidak pasti input field
+  // bisa jadi, button, heading dll
+  // gunakan <HTMLInputElement> agar useRef tau menggunakan html element
+  // alasan kenapa harus menggunakan null tiap useRef
+  // .current property pada ref object, mereferensikan pada Dom Node
+  // null value digunakan untuk akses ke DOM
+  // pertama kali saat menggunakan useRef tidak bisa akses ke DOM Node, jika kosong maka 'unidentified'
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submitted");
+    // gunakan if agar tidak error
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
   };
   return (
     <form className="p1-6-3 handling-form-submission" onSubmit={handleSubmit}>
@@ -12,14 +27,14 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       {/* div.mb-3>label.form-label+input[type=number].form-control */}
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       {/* button.btn.btn-primary cara menulis 2 className dalam 1 tag */}
       <button className="btn btn-primary" type="submit">
